4bfdbd3949305382693338ca05a08e381d89bb7f
Frederic F. Monfils <frederic.fleurialmonfils@cetic.be>
2015-02-07 20:01:06 +0100

    Implement dynamic creation of tasks

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,31 @@
 angular.module('starter.controllers', [])
-    .controller('ToDoListCtrl', function ($scope) {
-    $scope.toDoListItems = [{
-        task: 'Scuba Diving',
-        status: 'not done'
+    .controller('ToDoListCtrl', function ($scope, $ionicModal) {
+    $scope.toDoListItems = [];
+    $ionicModal.fromTemplateUrl('modal.html', function (modal) {
+    	$scope.modal = modal;
     }, {
-        task: 'Climb Everest',
-        status: 'not done'
-    }]
+    	scope: $scope,
+    	animation: 'slide-in-up'
+    });
+    // function to open the modal
+    $scope.openModal = function () {
+    	$scope.modal.show();
+    };
+    // function to close the modal
+    $scope.closeModal = function () {
+    	$scope.modal.hide();
+    };
+    // Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function () {
+    	$scope.modal.remove();
+    });
+    // function to add items to the existing list
+    $scope.AddItem = function (data) {
+    	$scope.toDoListItems.push({
+    		task: data.newItem,
+    		status: 'not done'
+    	});
+    	$scope.modal.hide();
+    	data.newItem = '';
+    };
 });
